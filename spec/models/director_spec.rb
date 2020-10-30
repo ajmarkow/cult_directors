a2626c62e021efcd9317708dfaf57eb87bd2100a
AJ Markow <alexanderjmarkow@gmail.com>
2020-10-30 15:47:40 -0700

    Remove tests for models I didnt have time to get to on 10/30

--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -1,5 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Director, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it("stores info like name, birthdate, and a director summary") do
+    newdirector = Director.new({ :name => "Stanley Kubrick", :birthday => "1928-07-26", :summary => "Great American Director" })
+    expect(newdirector.name()).to(eq("Stanley Kubrick"))
+  end
 end
