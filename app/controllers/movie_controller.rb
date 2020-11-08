09e33d88e5b4a2c4b75ae2f6be0cb4bcff0eca52
AJ Markow <alexanderjmarkow@gmail.com>
2020-11-08 15:02:09 -0800

    Add faker gem. Add  seeds.rb file to seed database with director records.

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,6 +1,39 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+    json_response(@movies)
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+    json_response(@movie)
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    json_response(@movie)
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    json_response(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    json_response("Movie with name " + @movie.name + " was deleted")
+  end
+
   private
+
   def json_response(object, status = :ok)
     render_json: object, status:status
+  end
+
+  def movie_params
+    params.permit(:title,:year_released,:summary,:movie_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  createddirector = Director.create!({:name => Faker::Movies::Lebowski.actor, :birthday =>Faker::Date.between(from: '1900-01-01', to: '2020-11-01'), :summary => Faker::Quote.robin})
+  puts createddirector
+end
